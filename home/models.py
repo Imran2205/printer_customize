2324653dd17bd4d860581ef71a67174afbdf7179
imran <imrankabir1996@gmail.com>
2021-04-06 11:56:46 +0600

    slider

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+"""class Actual_cash_flow_account_wise(models.Model):
+   " title = models.CharField(max_length=100)
+    gateway = models.CharField(max_length=100)
+    account_name = models.CharField(max_length=100)
+    incoming = models.FloatField()
+    outgoing = models.FloatField()
+    belonging_order_id = models.IntegerField()
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)"""
